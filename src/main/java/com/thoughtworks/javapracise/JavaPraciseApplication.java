d53400d3f44f62261a5595159d15e0939d290079
DQing <qqdou@thoughtworks.com>
2018-04-17 09:20:21 +0800

    init project

---
+++ src/main/java/com/thoughtworks/javapracise/JavaPraciseApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.javapracise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPraciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPraciseApplication.class, args);
+	}
+}
